60bded48edef8cdbf6a9fe40215b3a483121c2e0
Igor Moura <igor.oliveira.moura@gmail.com>
2025-02-23 20:32:21 -0300

    feat: Added newsletter

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import './index.css'
 import Analytics from './components/Analytics';
+import Newsletter from './components/Newsletter';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Navbar/>
       <Hero/>
       <Analytics/>
+      <Newsletter/>
     </div>
   );
 }
