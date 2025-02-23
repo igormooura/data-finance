1798eb25a84c10b6c1ad69f3a1f7e6e05d08a70f
Igor Moura <igor.oliveira.moura@gmail.com>
2025-02-23 19:38:58 -0300

    feat:Hero page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from './components/Navbar';
+import Hero from './components/Hero';
 import './index.css'
 
 function App() {
   return (
     <div>
       <Navbar/>
+      <Hero/>
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,8 +10,8 @@ const Navbar = () => {
 
   return (
     <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white'>
-      <h1 className='w-full text-3xl font-bold text-[#00df9a]'>REACT.</h1>
-      <ul className='hidden md:flex'>
+      <h1 className='w-full text-3xl font-bold text-[#00df9a]'>DATA FINANCE</h1>
+      <ul className='hidden md:flex'> {/*visible only in medium+ screens*/}
         <li className='p-4'>Home</li>
         <li className='p-4'>Company</li>
         <li className='p-4'>Resources</li>
@@ -21,11 +21,11 @@ const Navbar = () => {
 
       {/* For mobile */}
 
-      <div onClick={handleNav} className='block md:hidden'>
+      <div onClick={handleNav} className='block md:hidden'> {/* visible on mobile, hidden on medium+ screens*/}
           {nav ? <TfiClose size={20}/> : <TfiAlignLeft size={20} />}
       </div>
       <ul className={nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500' : 'ease-in-out duration-500 fixed left-[-100%]'}>
-        <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4'>REACT.</h1>
+        <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4'>DATA FINANCE</h1>
           <li className='p-4 border-b border-gray-600'>Home</li>
           <li className='p-4 border-b border-gray-600'>Company</li>
           <li className='p-4 border-b border-gray-600'>Resources</li>
