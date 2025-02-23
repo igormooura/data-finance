ad1e5b654b170cc55cddd5c272818890463f20c0
Igor Moura <igor.oliveira.moura@gmail.com>
2025-02-23 19:52:24 -0300

    feat: Added analytics and assets

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import './index.css'
+import Analytics from './components/Analytics';
 
 function App() {
   return (
     <div>
       <Navbar/>
       <Hero/>
+      <Analytics/>
     </div>
   );
 }
